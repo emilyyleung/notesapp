5c3a2ae959e1ca8e2cc6177aff8501c4865f19d2
emilyyleung <30677901+emilyyleung@users.noreply.github.com>
2022-03-07 20:25:21 +1100

    Part 11 - Utility Functions

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,41 @@
 import { Link } from "react-router-dom";
 
+let getTitle = (note) => {
+	const title = note.body.split("\n")[0];
+
+	if (title.length > 45) {
+		return title.slice(0, 45);
+	}
+
+	return title;
+};
+
+let getDate = (note) => {
+	return new Date(note.updated).toLocaleDateString();
+};
+
+let getContent = (note) => {
+	let content = note.body.split("\n");
+	content.shift();
+	content = content.join("\n");
+	content = content.replaceAll("\n", " ");
+
+	if (content.length > 45) {
+		return content.slice(0, 45);
+	}
+
+	return content;
+};
+
 const ListItem = ({ note }) => {
 	return (
 		<Link to={`/note/${note.id}`}>
 			<div className='notes-list-item'>
-				<h3>{note.body}</h3>
+				<h3>{getTitle(note)}</h3>
+				<p>
+					<span>{getDate(note)}</span>
+					{getContent(note)}
+				</p>
 			</div>
 		</Link>
 	);
